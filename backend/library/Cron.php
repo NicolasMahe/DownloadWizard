64d7db2d259131b164a4d91dc2b4a7521b69ab7b
Nico <nicolas@mahe.me>
2015-11-19 14:59:49 +0100

    Improvments

--- backend/library/Cron.php
+++ backend/library/Cron.php
@@ -10,57 +10,58 @@ class Cron
 		    }
 		    return ($a['completed'] > $b['completed']) ? -1 : 1;
 		}
-        
+
         $returnArray = array();
 
         $watchlist = new Table('watchlist');
-        $watchlistData = $watchlist->getAll();                
+        $watchlistData = $watchlist->getAll();
 		foreach($watchlistData as $watch)
 		{
-			if((!$watch['isDownloaded'] || (!empty($watch['nextSeason']) && !empty($watch['nextEpisode']))) && !empty($watch['tracker']))
+			if(((!$watch['isDownloaded'] || (!empty($watch['nextSeason']) && !empty($watch['nextEpisode']))) && !empty($watch['tracker'])) && (isset($watch['isActive']) && $watch['isActive']))
 			{
+
 				//construct search
 				$search = $watch['name'];
-				
+
 				if(!empty($watch['nextSeason']) && !empty($watch['nextEpisode']))
 					$search .= " S".sprintf("%02s", $watch['nextSeason'])."E".sprintf("%02s", $watch['nextEpisode']);
-					
+
 				if(!empty($watch['quality']))
 					$search .= " ".$watch['quality'];
-			
-			
-			
+
+
+
 				//do search on appropriate tracker
 				$tracker = new Tracker($watch['tracker']);
 				$data = $tracker->search($search);
-				
-				
-				
+
+
+
 				//sort array by completed
 				usort($data, 'cmp');
-				
+
 				if(count($data) > 0)
 				{
 					$returnArray[] = 'Match for "'.$search.'"';
-					
+
 					//take the first
 					$itemToDl = $data[0];
-					
+
 					Log::add('cron', 'match', 'A torrent match the search: "'.$search.'", torrent: "'.$itemToDl['title'].'"');
-					
+
 					$result = $tracker->download($itemToDl['downloadLink']);
-					
+
 					if($result)
 					{
 						//delete from watchlistCopy
 						$watch['isDownloaded'] = true;
 						$watch['dateDownloaded'] = date("Y-m-d H:i:s");
-						
+
 						if(!empty($watch['nextSeason']) && !empty($watch['nextEpisode']))
 						{
 							$watch['nextEpisode']++;
 						}
-                                                
+
                         $retour = $watchlist->update($watch);
 
                         if(!$retour)
@@ -85,14 +86,14 @@ class Cron
 				}
 			}
 		}
-		
+
 		$writeData['lastFetch'] = date("Y-m-d H:i:s");
-		
+
 		Storage::write(Config::get('storagePathCron'), $writeData, true);
-		
+
 		return $returnArray;
 	}
-        
+
     public function getLastFetch()
     {
         $data = Storage::read(Config::get('storagePathCron'), true);
@@ -102,17 +103,17 @@ class Cron
             return $data['lastFetch'];
         }
     }
-    
+
     public function extractFileAndDownloadSub($filepath, $name)
     {
         $pathTo = Recognize::pathToSave($name);
-        
-        
+
+
         $subtitle = new Subtitle('OpenSubtitles');
         $retourSub = $subtitle->downloadFirst($name);
-        
+
         Extract::archiveInPath($filepath.'/'.$name, $pathTo);
-        
+
         if($retourSub == true) {
             Log::add("cron", "extract", "success", "Cron extract for file '".$name."' success");
         } else {
--- frontend/app/module/watchlist/controller/watchlist.js
+++ frontend/app/module/watchlist/controller/watchlist.js
@@ -6,22 +6,23 @@ angular.module('module_watchlist')
 	$scope.watchListArray = null;
 	$scope.itemTmp = null;
 	$scope.lastFetch = "";
-	$scope.editMode = false;			
+	$scope.editMode = false;
 	$scope.isLoading = false;
-       
+
         $scope.dateShowIconDownloaded = moment().subtract('days', 3);
-	
+
 	$scope.showAdd = function()
 	{
 		$scope.editMode = false;
-		
+
 		$scope.itemTmp = {
-			'tracker': 'IPTorrents'
+			'tracker': 'IPTorrents',
+			'isActive': true
 		};
-		
+
 		$('#watchListModal').modal('show');
 	}
-	
+
 	$scope.doAdd = function(itemTmp)
 	{
             if($scope.editMode == true)
@@ -59,8 +60,8 @@ angular.module('module_watchlist')
                 });
             }
 	}
-        
-	
+
+
 	$scope.doDelete = function(itemTmp)
 	{
             $scope.isLoading = true;
@@ -75,26 +76,26 @@ angular.module('module_watchlist')
                 }
             });
 	}
-	
+
 	$scope.getWatchList = function(finishFunc)
 	{
 		$scope.isLoading = true;
-		
+
 		Watchlist.getAll(function(response) {
 			$scope.watchListArray = response.data;
-		
+
 			if(finishFunc)
                             finishFunc();
-                        else		
+                        else
                             $scope.isLoading = false;
 		});
 	}
-	
-	
+
+
 	$scope.fetchNow = function()
 	{
 		$scope.isLoading = true;
-		
+
 		Cron.doIt(function(response) {
 			$scope.getLastFetch(function(){
                             $scope.getWatchList(function() {
@@ -103,26 +104,26 @@ angular.module('module_watchlist')
 			});
 		});
 	}
-        
-        
+
+
 	$scope.getLastFetch = function(finishFunc)
 	{
 		$scope.isLoading = true;
-		
+
 		Cron.getLastFetch(function(response) {
                     $scope.lastFetch = response.data;
-                    
+
 			if(finishFunc)
                             finishFunc();
-                        else		
+                        else
                             $scope.isLoading = false;
-                            
+
 		});
 	}
-	
-	
-	
-	
+
+
+
+
 	$scope.showDelete = function(item, index, element)
 	{
 		var alert = confirm("Delete "+item.name+" ?");
@@ -131,19 +132,19 @@ angular.module('module_watchlist')
                     $scope.doDelete(item);
 		}
 	}
-	
-	
+
+
 	$scope.showEdit = function(item, index, element)
 	{
 		$scope.itemTmp = angular.copy(item);
 		$scope.itemTmpIndex = index;
-		
+
 		$scope.editMode = true;
-		
+
 		$('#watchListModal').modal('show');
 	}
-	
-	
+
+
 	$scope.initTracker = function() {
 		$scope.isLoading = true;
 		Tracker.getAll(function(response) {
@@ -151,10 +152,10 @@ angular.module('module_watchlist')
 			$scope.isLoading = false;
 		});
 	}
-	
-	
+
+
 	$scope.getWatchList();
 	$scope.initTracker();
         $scope.getLastFetch();
-	
+
 }]);
